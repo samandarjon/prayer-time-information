8dc83a6467e63ff04f3a8baab843c576c50b9321
samandarjon <45819610+samandarjon@users.noreply.github.com>
2020-01-27 11:29:26 +0500

    Update Keys.java

--- src/main/java/uz/pdp/config/Keys.java
+++ src/main/java/uz/pdp/config/Keys.java
@@ -1,6 +1,6 @@
 package uz.pdp.config;
 
 public class Keys {
-    public static String TOKEN = "978803867:AAE2hRf5sq88VOXgcG7dKiMMIHSgk-TtALQ";
-    public static String USERNAME = "saotBot";
+    public static String TOKEN = <Token>;
+    public static String USERNAME = <Username>;
 }
